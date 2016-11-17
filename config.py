1e8c80bb3acef5078d6f3cf5fcd85ca0a29dc60e
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-11-17 05:20:52 +0100

    Predefined constants and a simple config

---
+++ config.py
@@ -0,0 +1,5 @@
+AUTO_START_GAME = False
+SERVER_NAME = "hagesjo.se"
+SERVER_PORT = 8080
+SNAKE_NAME = "Snape"
+GAME_MODE = "training"
---
+++ constants.py
@@ -0,0 +1,18 @@
+# Inbound
+GAME_ENDED = "se.cygni.snake.api.event.GameEndedEvent"
+TOURNAMENT_ENDED = "se.cygni.snake.api.event.TournamentEndedEvent"
+MAP_UPDATE = "se.cygni.snake.api.event.MapUpdateEvent"
+SNAKE_DEAD = "se.cygni.snake.api.event.SnakeDeadEvent"
+GAME_STARTING = "se.cygni.snake.api.event.GameStartingEvent"
+PLAYER_REGISTERED = "se.cygni.snake.api.response.PlayerRegistered"
+INVALID_PLAYER_NAME = "se.cygni.snake.api.exception.InvalidPlayerName"
+HEART_BEAT_RESPONSE = "se.cygni.snake.api.response.HeartBeatResponse"
+GAME_LINK_EVENT = "se.cygni.snake.api.event.GameLinkEvent"
+GAME_RESULT_EVENT = "se.cygni.snake.api.event.GameResultEvent"
+
+# Outbound
+REGISTER_PLAYER_MESSAGE_TYPE = "se.cygni.snake.api.request.RegisterPlayer"
+START_GAME = "se.cygni.snake.api.request.StartGame"
+REGISTER_MOVE = "se.cygni.snake.api.request.RegisterMove"
+HEART_BEAT_REQUEST = "se.cygni.snake.api.request.HeartBeatRequest"
+CLIENT_INFO = "se.cygni.snake.api.request.ClientInfo"
