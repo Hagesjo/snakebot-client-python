0c66f523b386ab9dddd692a22c2b6e517cbd6f7b
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-11-19 17:11:14 +0100

    Fix wrong description and indentation error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from simplesnakebot import SimpleSnakebot
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="legget")
+    parser = argparse.ArgumentParser(description="Snakebot for cygni")
     parser.add_argument("-l", "--log", dest="loglevel",  help="Loglevel. DEBUG|INFO|WARNING|ERROR|CRITICAL", default="INFO")
     args = parser.parse_args()
 
@@ -11,7 +11,7 @@ def main():
     bot.register_player()
 
     while True:
-	bot.receive()
+        bot.receive()
 
 if __name__=="__main__":
     main()
